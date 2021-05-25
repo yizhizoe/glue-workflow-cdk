56f00a9cb9b0f96bb73f0a4f363ba2d822dd28cb
Zoe Gao <yizhigao@amazon.com>
2021-05-26 00:07:05 +0800

    move sample workflow to file

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 
 from aws_cdk import core
 
-from glue_workflow_cdk.glue_workflow_cdk_stack import GlueWorkflowCdkStack
+from glue_workflow_cdk.glue_workflow_cdk_stack import GlueJobCdkStack, GlueWorkflowCdkStack
 
 
 app = core.App()
+GlueJobCdkStack(app, "glue-job-cdk")
 GlueWorkflowCdkStack(app, "glue-workflow-cdk")
-
 app.synth()
--- glue_workflow_cdk/glue_workflow_cdk_stack.py
+++ glue_workflow_cdk/glue_workflow_cdk_stack.py
@@ -1,81 +1,78 @@
 from aws_cdk import core
 from aws_cdk.aws_glue import CfnTrigger, CfnWorkflow, CfnJob
-from aws_cdk import aws_s3 as s3
+from aws_cdk.aws_iam import Role, ServicePrincipal, ManagedPolicy
+import json
+import os
+INPUT_WORKFLOW_FILE = 'sample_workflow.json'
 
-WF_NAME = 'workflow2-by-cdk'
+SCRIPT_LOCATION = os.environ.get('GLUE_SCRIPT_S3')
 
-azk_flow = {
-  "project" : "azkaban-test-project",
-  "nodes" : [ {
-    "id" : "test-final",
-    "type" : "command",
-    "in" : [ "test-job-4" ]
-  },
-      {
-    "id" : "test-job-start",
-    "type" : "java"
-  }, {
-    "id" : "test-job-3",
-    "type" : "java",
-    "in" : [ "test-job-2" ]
-  }, {
-    "id" : "test-job-4",
-    "type" : "java",
-    "in" : [ "test-job-3" ]
-  }, {
-    "id" : "test-job-2",
-    "type" : "java",
-    "in" : [ "test-job-start" ]
-  } ],
-  "flow" : "test",
-  "projectId" : 193
-}
+class GlueJobCdkStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        with open(INPUT_WORKFLOW_FILE) as f:
+            workflow_dict = json.load(f)
+            glue_role = Role(self, "dummy_glue_job_role",
+                             assumed_by=ServicePrincipal("glue.amazonaws.com"),
+                             managed_policies=[ManagedPolicy.from_aws_managed_policy_name("service-role/AWSGlueServiceRole")],
+                             )
+            # mock job
+            job_command = CfnJob.JobCommandProperty(name='pythonshell',
+                                                    script_location=SCRIPT_LOCATION)
+
+            for workflow_item in workflow_dict:
+                for node in workflow_item['nodes']:
+                    triggered_job_name = node['id']
+                    job = CfnJob(self, triggered_job_name,
+                                 name=triggered_job_name,
+                                 command=job_command,
+                                 role=glue_role.role_name)
+
 
 class GlueWorkflowCdkStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        CfnWorkflow(self, WF_NAME, name=WF_NAME)
+        with open(INPUT_WORKFLOW_FILE) as f:
+            workflow_dict = json.load(f)
 
-        job_command = CfnJob.JobCommandProperty(name='pythonshell',
-                                                script_location="s3://aws-glue-scripts-182470276418-cn-north-1/yizhi/dummy.py")
-        last_trigger = None
+            for workflow_item in workflow_dict:
+                workflow_name = workflow_item['flow']
+                CfnWorkflow(self, workflow_name, name=workflow_name)
 
-        for node in azk_flow['nodes']:
+                last_trigger = None
 
-            triggered_job_name = node['id']
+                for node in workflow_item['nodes']:
 
+                    triggered_job_name = node['id']
 
-            job = CfnJob(self, triggered_job_name,
-                         name=triggered_job_name,
-                         command=job_command,
-                         role='glue-readmsk')
+                    action_property = CfnTrigger.ActionProperty(job_name=triggered_job_name)
 
-            action_property = CfnTrigger.ActionProperty(job_name=triggered_job_name)
+                    if 'in' not in node:
+                        # Scheduled trigger
+                        trigger = CfnTrigger(self, 'scheduled-daily-trigger', workflow_name=workflow_name,
+                                             actions=[action_property],
+                                             start_on_creation=True,
+                                                    type='SCHEDULED',
+                                                    schedule='cron(00 23 * * ? *)')
+                    else:
+                        # Trigger on other job success
+                        firing_on_job_list = node['in']
+                        condition_list = [CfnTrigger.ConditionProperty(job_name=firing_job,
+                                                                       state='SUCCEEDED',
+                                                                       logical_operator='EQUALS')
+                                          for firing_job in firing_on_job_list]
 
-            if 'in' not in node:
-                # Scheduled trigger
-                trigger = CfnTrigger(self, 'daily-trigger-2', workflow_name=WF_NAME, actions=[action_property],
-                                            type='SCHEDULED',
-                                            schedule='cron(00 14 * * ? *)')
-            else:
-                # Trigger on other job success
-                firing_on_job_list = node['in']
-                condition_list = [CfnTrigger.ConditionProperty(job_name=firing_job,
-                                                               state='SUCCEEDED',
-                                                               logical_operator='EQUALS')
-                                  for firing_job in firing_on_job_list]
+                        firing_predicate=CfnTrigger.PredicateProperty(conditions=condition_list, logical='AND')
 
-                firing_predicate=CfnTrigger.PredicateProperty(conditions=condition_list, logical='AND')
-
-                trigger = CfnTrigger(self, 'trigger-'+triggered_job_name,
-                                         workflow_name=WF_NAME,
-                                         actions=[action_property],
-                                         type='CONDITIONAL',
-                                         predicate=firing_predicate)
-            if last_trigger:
-                last_trigger.add_depends_on(trigger)
-            last_trigger = trigger
-
-        # The code that defines your stack goes here
+                        trigger = CfnTrigger(self, 'trigger-'+triggered_job_name,
+                                                 workflow_name=workflow_name,
+                                                 actions=[action_property],
+                                                 start_on_creation=True,
+                                                 type='CONDITIONAL',
+                                                 predicate=firing_predicate)
+                    # if last_trigger:
+                    #     trigger.add_depends_on(last_trigger)
+                    # last_trigger = trigger
